6c9fca4ab7b58e3927518fd2bb756aa7743f6b98
troyesguerra <troyesguerra@gmail.com>
2022-01-18 22:25:52 +0800

    frontend sections commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const container = document.querySelector('.container')
 hamburger.addEventListener('click', () => {
     container.classList.toggle('change')
 })
+
+// Scroll Buton Functionality
+
+const scrollButton = document.querySelector('.scroll-btn');
+scrollButton.addEventListener('click', () => {
+    document.querySelector('html').style.scrollBehavior = 'smooth';
+    setTimeout(() => {
+        document.querySelector('html').style.scrollBehavior = 'unset';
+    }, 1000)
+})
