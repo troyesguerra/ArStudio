59c844a183c470681d0c0240447dfed1e7f00647
troyesguerra <troyesguerra@gmail.com>
2022-01-19 15:58:22 +0800

    additional frontend changes commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add('display')
+    }, 4000);
+}
+
+
 const hamburger = document.querySelector('.hamburger-menu');
 const container = document.querySelector('.container')
 
